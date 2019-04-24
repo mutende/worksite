7adfca3ecd2d7f3d02b4507cefa5a164a1253e39
Elvis Mutende <elvismutende@gmail.com>
2019-04-24 12:35:30 +0300

    refinement on landingpage

--- worksite/settings.py
+++ worksite/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'worksite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'worksite',
+        'USER': 'mutende',
+        'PASSWORD': 'm0910',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
