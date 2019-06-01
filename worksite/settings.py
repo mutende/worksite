83a2f50b15342fa2c1e30cefd6606b93938fc11b
Elvis Mutende <elvismutende@gmail.com>
2019-06-01 13:10:57 +0300

    url settings

--- worksite/settings.py
+++ worksite/settings.py
@@ -94,6 +94,7 @@ WSGI_APPLICATION = 'worksite.wsgi.application'
 #         'PORT': '3306'
 #     }
 # }
+
 #remotedatabase
 DATABASES = {
     'default': {
@@ -161,3 +162,25 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedorReadOnly'
     ]
 }
+
+#for local
+# CORS_REPLACE_HTTPS_REFERER      = False
+# HOST_SCHEME                     = "http://"
+# SECURE_PROXY_SSL_HEADER         = None
+# SECURE_SSL_REDIRECT             = False
+# SESSION_COOKIE_SECURE           = False
+# CSRF_COOKIE_SECURE              = False
+# SECURE_HSTS_SECONDS             = None
+# SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+# SECURE_FRAME_DENY               = False
+
+#for deployed
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
